cc8039303e2d2618650bab28e962873d79a0d527
Niyamaka <niyamaka@yuneta.io>
2021-06-11 11:56:12 +0200

    4.13.1

--- src/main.c
+++ src/main.c
@@ -51,7 +51,7 @@ static error_t parse_opt (int key, char *arg, struct argp_state *state);
 #define APP_NAME        "ytests"
 #define APP_DOC         "Yuneta Tests"
 
-#define APP_VERSION     "4.13.0"
+#define APP_VERSION     "4.13.1"
 #define APP_DATETIME    __DATE__ " " __TIME__
 #define APP_SUPPORT     "<niyamaka at yuneta.io>"
 
