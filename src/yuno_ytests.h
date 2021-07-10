979ba035617884b03fa9aa30ab1c46f4be8ea2d4
Niyamaka <niyamaka@yuneta.io>
2021-07-10 19:55:10 +0200

    pragma once

--- src/yuno_ytests.h
+++ src/yuno_ytests.h
@@ -5,8 +5,7 @@
  *          Copyright (c) 2016 Niyamaka.
  *          All Rights Reserved.
  ****************************************************************************/
-#ifndef _YUNO_YTESTS_H
-#define _YUNO_YTESTS_H 1
+#pragma once
 
 #include <yuneta.h>
 
@@ -24,7 +23,4 @@ PUBLIC void register_yuno_ytests(void);
 
 #ifdef __cplusplus
 }
-#endif
-
-
 #endif
