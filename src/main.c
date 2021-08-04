c847b61bf2c2f519312301aed836c149fe6d8d48
Niyamaka <niyamaka@yuneta.io>
2021-08-04 13:55:00 +0200

    add oauth2 parameters to console apps

--- src/main.c
+++ src/main.c
@@ -32,6 +32,10 @@ struct arguments
     char *yuno_name;
     char *yuno_service;
 
+    char *token_endpoint;
+    char *user_id;
+    char *jwt;
+
     int verbose;                /* verbose */
     int print;
     int print_version;
@@ -182,8 +186,13 @@ static error_t parse_opt (int key, char *arg, struct argp_state *state)
 
     switch (key) {
     case 'e':
+        arguments->token_endpoint = arg;
         break;
     case 'x':
+        arguments->user_id = arg;
+        break;
+    case 'j':
+        arguments->jwt = arg;
         break;
 
     case 't':
